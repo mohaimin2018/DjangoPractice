f08085a5bd7a6cf3f2e6b00ff8a715b8f54a1b63
mohai <mohai.cse@gmail.com>
2023-10-21 22:34:12 +0600

    templates are configured.

--- demo_2/demo_2/settings.py
+++ demo_2/demo_2/settings.py
@@ -15,9 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR)
 
-
+ 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'demo_2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- demo_2/teacher/views.py
+++ demo_2/teacher/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def details(request):
+
+    return render(request,'teacher/templates/index.html')
