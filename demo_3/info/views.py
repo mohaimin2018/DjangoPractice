ac9bf90b33530c2fa6bf4710e1febe9aeb151ce0
mohai <mohai.cse@gmail.com>
2023-10-22 21:30:40 +0600

    Update template with DTL

--- demo_3/info/views.py
+++ demo_3/info/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def details(request):
+
+    name = 'Mohai'
+    age = 27
+    gender = 'Male'
+
+    mydict = {'name':name, 'age':age, 'gender':gender}
+
+    return render(request,'info/index.html',context=mydict)
